86ca9f67291f7d3eb77a70f9ade0bb053d7c4edd
CaciaRodrigues <caciarodriguues@gmail.com>
2025-01-28 13:58:10 -0300

    Mudando cores

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
 
     return (
         <>
-            <div className="flex justify-center bg-gray-900 text-gray-100">
+            <div className="flex justify-center bg-gray-900 text-gray-200">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
                             Gen Fit - PI Grupo 5 | Copyright: {data}
@@ -14,7 +14,7 @@ function Footer() {
                     <p className='text-lg'>Para mais de nossos projetos:</p>
                     <div className='flex gap-2'>
                         <a href="https://github.com/Grupo5-PrjetoIntegrador">
-                            <GithubLogo size={48} weight='bold' className='hover:text-slate-400' />
+                            <GithubLogo size={48} weight='bold' className='hover:text-accent-color' />
                         </a>
                     </div>
                 </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,13 +4,13 @@ function Navbar() {
     return (
         <>
             <div className='w-full flex justify-center py-4
-            			   bg-gray-900 text-gray-100'>
+            			   bg-gray-900 text-gray-200'>
             
                 <div className="container flex justify-between text-lg">
                     <h1 className="text-3xl font-bold">Gen Fit</h1>
                     <div className='flex gap-4 text-2xl'>
-                        <p className="hover:text-slate-400">Sobre Nós</p>
-                        <p className="hover:text-slate-400">Produto</p>
+                        <p className="hover:text-accent-color">Sobre Nós</p>
+                        <p className="hover:text-accent-color">Produto</p>
                     </div>
                 </div>
             </div>
