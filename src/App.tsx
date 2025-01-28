f0da15dcaea9f87dcd84f0aa188ab4fd0a423fac
CaciaRodrigues <caciarodriguues@gmail.com>
2025-01-28 14:25:37 -0300

    Adicionando rotas ao App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import SobreNos from "./pages/sobrenos/SobreNos";
+import ExerciseOptions from "./pages/produto/Produto";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[-80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/sobrenos" element={<SobreNos />} />
+            <Route path="/produto" element={<ExerciseOptions />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
